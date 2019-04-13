3eec37ea24129b6924bf8037902d14f80e897656
aroy34 <300853516@cencol.ca>
2019-04-13 17:25:02 -0400

    configureation change

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports={
-    //"URI" : "mongodb://localhost/project"
-    "URI" : "mongodb+srv://admin:admin@cluster0-aly8r.mongodb.net/hospitaldb?retryWrites=true"
+    "URI" : "mongodb://localhost/project"
+    //"URI" : "mongodb+srv://admin:admin@cluster0-aly8r.mongodb.net/hospitaldb?retryWrites=true"
     
 }
